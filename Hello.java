518c0489a7fc168306e4829d46378c487aa66222
faridmuarif <fmfarid30@gmail.com>
2021-03-22 20:41:02 +0800

    Hello Git & Java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
  public static void main (String [] args){
-  System.out.prinln("Hello World !!");
+  System.out.println("Hello Git !!");
+  System.out.println("Hello World !!");
  }
 }
